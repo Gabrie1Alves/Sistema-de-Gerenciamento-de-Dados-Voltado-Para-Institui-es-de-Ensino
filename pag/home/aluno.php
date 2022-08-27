6fdcdcc43fa637c10d12cad3cde53e8ce5c2c86a
Gabrie1Alves <gabrielalvesresenderocha@gmail.com>
2022-08-27 17:13:45 -0300

    Correção de footer + desenvolvimento de páginas home de aluno, professor e gerenciador (estáticas)

--- pag/home/aluno.php
+++ pag/home/aluno.php
@@ -5,12 +5,60 @@
 
         <!-- CSS -->
         <link rel="stylesheet" href="../../css/geral/css.css">
+        <link rel="stylesheet" href="../../css/todos/home_aluno-prof-geren.css">
     </head>
     <body>
         <?php include_once '../../header_footer/header.php'?>
 
         <div class="container">
-            
+            <div class="apresentacao_home">
+                Olá {{nome}}, bem vindo ao painel do Aluno! A seguir, estão todas as ações que você pode executar!
+            </div>
+            <div class="secao_opcoes">
+                <a href="">
+                    <div class="op">
+                        teste
+                        <span class="op_explicacao">Suspendisse sapien nisl, ultrices semper nulla sit amet</span>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="op">
+                        teste
+                        <span class="op_explicacao">Suspendisse sapien nisl, ultrices semper nulla sit amet</span>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="op">
+                        teste
+                        <span class="op_explicacao">Suspendisse sapien nisl, ultrices semper nulla sit amet</span>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="op">
+                        teste
+                        <span class="op_explicacao">Suspendisse sapien nisl, ultrices semper nulla sit amet</span>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="op">
+                        teste
+                        <span class="op_explicacao">Suspendisse sapien nisl, ultrices semper nulla sit amet</span>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="op">
+                        teste
+                        <span class="op_explicacao">Suspendisse sapien nisl, ultrices semper nulla sit amet</span>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="op">
+                        teste
+                        <span class="op_explicacao">Suspendisse sapien nisl, ultrices semper nulla sit amet</span>
+                    </div>
+                </a>
+
+            </div>
         </div>
 
         <?php include_once '../../header_footer/footer.php'?>
