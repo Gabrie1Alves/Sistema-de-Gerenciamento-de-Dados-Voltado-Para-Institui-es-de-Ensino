b589db95b54c42eb71202ad901463c76b83fd34d
Gabriel <gabrielalvesresenderocha@gmail.com>
2022-10-29 23:46:01 -0300

    implementação de inserção em turma_notas + exclusão

--- pag/professor/_criar-atividade.php
+++ pag/professor/_criar-atividade.php
@@ -23,12 +23,48 @@
             echo "ERRO NO CADASTRO DE ENDEREÇO!";
             echo "Alguma coisa deu errado no banco :O";
         } 
+
+        //busca id atividade
+        $id_atividade = "SELECT MAX(id) from atividades_avaliativas";
+        $id_atividade = mysqli_query($conn, $id_atividade);
+        $id_atividade = $id_atividade->fetch_assoc();
+        $id_atividade = $id_atividade['MAX(id)'];
+
+        $sql = "SELECT matricula FROM aluno where turma = '$info[0]'";
+        $sql = mysqli_query($conn, $sql);
+        $aux = 0;
+        if($sql->num_rows > 0){
+            while($v = $sql->fetch_assoc()){
+                $mat_aluno[$aux] = $v['matricula'];
+                $aux = $aux + 1; 
+            }
+        }
+        $x = "";
+        for($i = 0; $i < count($mat_aluno); $i++){
+            if($i == count($mat_aluno)-1){
+                $x = $x."('$info[0]','$info[1]','$id_atividade', '$mat_aluno[$i]', '$info[6]', 0)";
+            }else{
+                $x = $x."('$info[0]','$info[1]','$id_atividade', '$mat_aluno[$i]', '$info[6]', 0),";
+            }
+        }
+
+        $sql = "INSERT INTO turma_notas(sigla_turma, sigla_disc, id_atividade, mat_aluno, valor_atividade, valor_atingido)
+                VALUES $x";
+        if(!mysqli_query($conn, $sql)){
+            echo "ERRO NO CADASTRO DE ENDEREÇO!";
+            echo "Alguma coisa deu errado no banco :O";
+        } 
     }else{
         $sql = "DELETE FROM atividades_avaliativas WHERE id = '$id'";
         if(!mysqli_query($conn, $sql)){
             echo "ERRO NO CADASTRO DE ENDEREÇO!";
             echo "Alguma coisa deu errado no banco :O";
-        } 
+        }
+        $sql = "DELETE FROM turma_notas where id_atividade = $id";
+        if(!mysqli_query($conn, $sql)){
+            echo "ERRO NO CADASTRO DE ENDEREÇO!";
+            echo "Alguma coisa deu errado no banco :O";
+        }
     }
     
 ?>
